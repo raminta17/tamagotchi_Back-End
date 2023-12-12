d82de66299583a7041cab1be117cd3437404c95b
Raminta Alisauskaite <raminta.alisauskaite@gmail.com>
2023-12-12 14:46:55 +0200

    cors access control added

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-const express = require('express');
-const cors = require('cors');
-const router = require('./routers/mainRouters');
-const app = express();
-const port = 8000;
+const express = require('express')
+const cors = require('cors')
+const router = require('./routers/mainRouters')
+const app = express()
+const port = 8000
+const corsOptions = {
+	origin: '*',
+	credentials: true,
+	optionSuccessStatus: 200,
+}
 
+app.use(cors(corsOptions))
+app.use(express.json())
+app.use('/', router)
 
-app.use(cors());
-app.use(express.json());
-app.use("/", router);
-
-app.listen(port);
+app.listen(port)
